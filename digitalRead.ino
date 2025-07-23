cc6de1e7ae3a86dd45c669b01bda84cfc874d06f
MOHAN SAI <143937966+MOHAN-009@users.noreply.github.com>
2025-07-23 16:18:48 +0530

    Add files via upload

---
+++ digitalRead.ino
@@ -0,0 +1,13 @@
+int inputPin = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(inputPin, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+Serial.println(digitalRead(inputPin));
+delay(500);
+}
